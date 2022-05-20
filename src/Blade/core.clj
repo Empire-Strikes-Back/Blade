91116dec6fdafe8bc264278f7fbe67824f337c55
Blade <Blade@daywalker>
2022-05-20 18:29:13 +0300

    some motherfuckers are always trying to ice-skate uphill

---
+++ src/Blade/core.clj
@@ -0,0 +1,17 @@
+(ns Dooku.seed
+  (:require
+   [clojure.core.async :as Little-Rock
+    :refer [chan put! take! close! offer! to-chan! timeout thread
+            sliding-buffer dropping-buffer
+            go >! <! alt! alts! do-alts
+            mult tap untap pub sub unsub mix unmix admix
+            pipe pipeline pipeline-async]]
+   [clojure.java.io :as Wichita.java.io]
+   [clojure.string :as Wichita.string]))
+
+(do (set! *warn-on-reflection* true) (set! *unchecked-math* true))
+
+(defn process
+  [opts]
+  (let [default-opts {}
+        {:keys []} (merge default-opts opts)]))
